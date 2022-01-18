c59525787255e9a1dbe791408710ea004f2ee94e
Krzysztof Kiec <kkiec95@gmail.com>
2022-01-18 19:17:43 +0100

    Add stripe checkout, prices, billing portal

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
+  get 'pricing', to: 'static_pages#pricing'
   resources :posts
+  post 'checkout/create', to: 'checkout#create', as: 'checkout_create'
+  post 'billing_portal/create', to: 'billing_portal#create', as: 'billing_portal_create'
 end
