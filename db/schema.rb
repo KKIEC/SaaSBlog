6fb0338c089dfe12a7a98c26576e7e2e77bb56dc
Krzysztof Kiec <kkiec95@gmail.com>
2022-01-18 13:47:39 +0100

    Switch database to postgresql, add seedwith gem faker

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_18_122411) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
