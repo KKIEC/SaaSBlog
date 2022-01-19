f190714cd3d480979d8d4cbd691275fa3c25670b
Krzysztof Kiec <kkiec95@gmail.com>
2022-01-19 15:16:01 +0100

    Add current_period_end to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_145214) do
+ActiveRecord::Schema.define(version: 2022_01_19_140951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_145214) do
     t.string "plan"
     t.string "subscription_status", default: "incomplete"
     t.string "stripe_customer_id"
+    t.datetime "current_period_end"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
